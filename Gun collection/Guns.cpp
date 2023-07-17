cb792e02126a4de9bcf13c45c2057425ab3431ee
RealFrikas <100031996+RealFrikas@users.noreply.github.com>
2023-07-17 16:40:43 +0300

    Add files via upload

--- Gun collection/Guns.cpp
+++ Gun collection/Guns.cpp
@@ -5,14 +5,26 @@ class Gun{
         std::string name;
         int maxammo;
         int magammo;
-
+        int mode = 0;
+        bool single,burst,automatic;
     public:
 
-    //constructs gun with name,capacity and a loaded mag
-    Gun(std::string n,int maxa){
+    //constructs gun with name,capacity,a loaded mag , and available modes
+    Gun(std::string n,int maxa,bool s,bool b,bool a){
         name = n;
         maxammo = maxa;
         magammo = maxa;
+        single = s;
+        burst = b;
+        automatic = a;
+        if(s == false){
+            if(b == true){
+                mode = 1;
+            }
+            else{
+                mode = 2;
+            }
+        }
     }
 
     std::string Getname(){
@@ -23,14 +35,34 @@ class Gun{
         return magammo;
     }
 
+    void Changemode(){//cycle modes
+        if(single == true && ((mode == 1 && automatic == false) || (mode == 2))){
+            mode = 0;
+            std::cout << "Mode changed to single shot!" << std::endl;
+        }
+        else if(burst == true && ((mode == 2 && single == false) || (mode == 0))){
+            mode = 1;
+            std::cout << "Mode changed to burst!" << std::endl;
+        }
+        else if (automatic == true && ((mode == 0 && burst == false) || (mode == 1))){
+            mode = 2;
+            std::cout << "Mode changed to automatic!" << std::endl;
+        }
+        else{
+            std::cout << "Cannot change mode!" << std::endl;
+        }
+    }
+
     //checks if mag has bullets,fires
     void Fire(){
+        //time between pew
         if(magammo > 0){
+            //string soundeffect
             std::cout<<"pew"<<std::endl;
             magammo--;
         }
         else{
-            std::cout<<"clack,out of ammo"<<std::endl;
+            std::cout<<"clack"<<std::endl;
         }
     }
 
@@ -87,8 +119,9 @@ int main(){
 
     
     Gun guns[]{
-           Gun("Revolver",6),
-           Gun("M1911",20)
+           Gun("Revolver",6,true,false,false),
+           Gun("M1911",7,true,false,false),
+           Gun("FAMAS",30,false,true,true)
     };
 
     //find how many guns we got and runs the function
@@ -98,11 +131,11 @@ int main(){
     bool exit = true;
     int ans;
     std::cout << "You can fire,reload,see how much ammo is left by typing:\n"
-    << "0:Exit\n1:Fire\n2:reload\n3:check\n4:change gun" <<std::endl;
+    << "0:Exit\n1:Fire\n2:reload\n3:check\n4:change gun\n5:Change mode" <<std::endl;
     do{
     	int ans;
         do{
-			ans=getUNum(5);
+			ans=getUNum(6);
 			//debug
 			//std::out << ans << std::endl;
 		}while(ans<0);
@@ -124,6 +157,9 @@ int main(){
                 selectedgun=Selectgun(guns,sizeofguns);
                 std::cout << "You can fire,reload,see how much ammo is left by typing:\n"
                 << "0:Exit\n1:Fire\n2:reload\n3:check\n4:change gun" <<std::endl;
+                break;
+            case 5:
+                guns[selectedgun].Changemode();
                 break;
         }
     }while(exit);
